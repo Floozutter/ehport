a3c16738c9923a669cc251afd678262b0f3eaa11
Floozutter <floozutter@gmail.com>
2021-02-07 00:01:42 -0800

    get login credentials

--- ehex.py
+++ ehex.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from getpass import getpass
 
 if __name__ == "__main__":
     driver = webdriver.Firefox()
     driver.get("https://e-hentai.org/favorites.php")
+    username = input("username: ")
+    password = getpass("password: ")    
