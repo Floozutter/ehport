db8b6c0be5813bf1e053aa9c1d494a05b4934267
Floozutter <floozutter@gmail.com>
2021-02-08 20:10:03 -0800

    enter username and password

--- ehex.py
+++ ehex.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from getpass import getpass
 
 if __name__ == "__main__":
     driver = webdriver.Firefox()
-    driver.get("https://e-hentai.org/favorites.php")
-    username = input("username: ")
-    password = getpass("password: ")    
-    del password
+    driver.get("https://e-hentai.org/bounce_login.php?b=d&bt=1-6")
+    driver.find_element_by_name("UserName").send_keys(input("username: "))
+    driver.find_element_by_name("PassWord").send_keys(getpass("password: "))
