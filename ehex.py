a01f18c89f6ab5d664820c1db654c25725a78152
Floozutter <floozutter@gmail.com>
2021-02-18 15:33:06 -0800

    refactor

--- ehex.py
+++ ehex.py
@@ -1,11 +1,34 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from getpass import getpass
+from typing import NamedTuple
 
-if __name__ == "__main__":
-    driver = webdriver.Firefox()
+class Favorite(NamedTuple):
+    gid: str
+    t: str
+    color_title: str
+
+def login(driver, username: str, password: str) -> None:
     driver.get("https://e-hentai.org/bounce_login.php")
     driver.find_element_by_name("UserName").send_keys(input("username: "))
     driver.find_element_by_name("PassWord").send_keys(getpass("password: "))
     driver.find_element_by_name("ipb_login_submit").click()
-    driver.get("https://e-hentai.org/favorites.php?inline_set=fs_f")
+
+def set_modes(driver) -> None:
+    driver.get("https://e-hentai.org/favorites.php?inline_set=fs_f")  # favorited order
+    driver.get("https://e-hentai.org/favorites.php?inline_set=dm_m")  # minimal display mode
+
+def get_pages(driver) -> int:
+    driver.get("https://e-hentai.org/favorites.php")
+    return int(driver.find_elements_by_css_selector("table.ptt td")[-2].text)
+
+def get_favorites(driver, page: int) -> tuple[Favorite, ...]:
+    driver.get(f"https://e-hentai.org/favorites.php?page={p}")
+    return ()
+
+if __name__ == "__main__":
+    d = webdriver.Firefox()
+    login(d, input("username: "), getpass("password: "))
+    set_modes(d)
+    pages = get_pages(d)
+    favorites = (f for p in range(pages)[::-1] for f in get_favorites(driver, p))
