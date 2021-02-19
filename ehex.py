582a52165576f69c2ef687a13f20ed9dcb7b6910
Floozutter <floozutter@gmail.com>
2021-02-18 17:16:34 -0800

    implement get_favorites

--- ehex.py
+++ ehex.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from urllib.parse import urlparse
 from getpass import getpass
 from typing import NamedTuple
 
@@ -23,12 +24,18 @@ def get_pages(driver) -> int:
     return int(driver.find_elements_by_css_selector("table.ptt td")[-2].text)
 
 def get_favorites(driver, page: int) -> tuple[Favorite, ...]:
-    driver.get(f"https://e-hentai.org/favorites.php?page={p}")
-    return ()
+    driver.get(f"https://e-hentai.org/favorites.php?page={page}")
+    rows = driver.find_elements_by_css_selector("table.itg.gltm tr:not(:first-of-type)")
+    def parse(row) -> Favorite:
+        url = row.find_element_by_css_selector("td.gl3m.glname a").get_attribute("href")
+        _, gid, t = filter(None, urlparse(url).path.split("/"))
+        ct = row.find_element_by_css_selector("td.gl2m > div:last-child").get_attribute("title")
+        return Favorite(gid, t, ct)
+    return tuple(map(parse, rows))
 
 if __name__ == "__main__":
     d = webdriver.Firefox()
     login(d, input("username: "), getpass("password: "))
     set_modes(d)
     pages = get_pages(d)
-    favorites = (f for p in range(pages)[::-1] for f in get_favorites(driver, p))
+    favorites = tuple(f for p in range(pages)[::-1] for f in get_favorites(d, p))
