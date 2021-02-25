8bf8c31a641068802d39aa76897813469492ae4a
Floozutter <floozutter@gmail.com>
2021-02-24 16:55:33 -0800

    export to csv

--- common.py
+++ common.py
@@ -1,14 +1,18 @@
 from requests_html import HTMLSession, HTMLResponse
-from enum import Enum
+from csv import unix_dialect
 from typing import NamedTuple
-
-URL_LOGIN = "https://forums.e-hentai.org/index.php?act=Login&CODE=01"
-URL_FAV = "https://e-hentai.org/favorites.php"
 
 class Favorite(NamedTuple):
     gid: str
     t: str
     color_title: str
+
+CSV_FIELDNAMES = Favorite._fields
+class CSV_DIALECT(unix_dialect):
+    pass
+
+URL_LOGIN = "https://forums.e-hentai.org/index.php?act=Login&CODE=01"
+URL_FAVS = "https://e-hentai.org/favorites.php"
 
 def login(session: HTMLSession, username: str, password: str) -> HTMLResponse:
     data = {
@@ -21,5 +25,5 @@ def login(session: HTMLSession, username: str, password: str) -> HTMLResponse:
     return session.post(URL_LOGIN, data)
 
 def pry(filename: str, text: str) -> None:
-    with open(filename, "w", encoding = "utf-8") as f:
+    with open(filename, mode = "w", encoding = "utf-8") as f:
         f.write(text)
--- ehex.py
+++ ehex.py
@@ -1,18 +1,22 @@
-from common import URL_FAV, Favorite, login
+from common import Favorite, CSV_FIELDNAMES, CSV_DIALECT, URL_FAVS, login
 from requests_html import HTMLSession, HTMLResponse
 from urllib.parse import urlparse
+from csv import DictWriter
+from time import sleep
 from getpass import getpass
 
+SECONDS_PER_PAGE = 1
+
 def inline_set(session: HTMLSession, var: str, val: str) -> HTMLResponse:
-    return session.get(URL_FAV, params = {"inline_set": f"{var}_{val}"})
+    return session.get(URL_FAVS, params = {"inline_set": f"{var}_{val}"})
 
 def scrape_pagecount(session: HTMLSession) -> int:
-    r = session.get(URL_FAV)
+    r = session.get(URL_FAVS)
     pages = r.html.find("table.ptt td")
     return int(pages[-2].text)
 
 def scrape_favorites(session: HTMLSession, page: int) -> tuple[Favorite, ...]:
-    r = session.get(URL_FAV, params = {"page": page})
+    r = session.get(URL_FAVS, params = {"page": page})
     rows = r.html.find("table.itg.gltm tr")[1:]
     def parse(row) -> Favorite:
         url = row.find("td.gl3m.glname a", first = True).attrs["href"]
@@ -22,10 +26,18 @@ def scrape_favorites(session: HTMLSession, page: int) -> tuple[Favorite, ...]:
     return tuple(map(parse, rows))
 
 if __name__ == "__main__":
-    s = HTMLSession()
-    login(s, input("username: "), getpass("password: "))
-    s.get(URL_FAV)  # get some more cookies to munch on
-    inline_set(s, "fs", "f")  # sort by favorites
-    inline_set(s, "dm", "m")  # minimal display mode
-    pagecount = scrape_pagecount(s)
-    #favorites = tuple(f for p in range(pages)[::-1] for f in get_favorites(d, p))
+    session = HTMLSession()
+    login(session, input("username: "), getpass("password: "))
+    session.get(URL_FAVS)  # get some more cookies to munch on
+    inline_set(session, "fs", "f")  # sort by favorites
+    inline_set(session, "dm", "m")  # minimal display mode
+    pagecount = scrape_pagecount(session)
+    with open(input("output file: "), mode = "w") as ofile:
+        writer = DictWriter(ofile, fieldnames = CSV_FIELDNAMES, dialect = CSV_DIALECT)
+        writer.writeheader()
+        for page in range(pagecount)[::-1]:
+            favorites = scrape_favorites(session, page)[::-1]
+            writer.writerows(map(lambda fav: fav._asdict(), favorites))
+            print(f"scraped {len(favorites)} favorites from page {page}.")
+            sleep(SECONDS_PER_PAGE)
+    print("all done. >:3c")
