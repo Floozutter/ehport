0f7d47ce7a9e14ecea95b196830e63055138aaa2
Floozutter <floozutter@gmail.com>
2021-02-24 17:17:31 -0800

    favorite Favorite with favorite_favorite

--- ehim.py
+++ ehim.py
@@ -1,10 +1,13 @@
 from common import Favorite, CSV_DIALECT, login
 from requests_html import HTMLSession, HTMLResponse
 from csv import DictReader
+from time import sleep
 from getpass import getpass
-from typing import Tuple
 
 SECONDS_PER_FAV = 1
+
+def favorite_favorite(session: HTMLSession, favorite: Favorite) -> HTMLResponse:
+    raise NotImplementedError
 
 if __name__ == "__main__":
     session = HTMLSession()
@@ -12,6 +15,8 @@ if __name__ == "__main__":
     with open(input("input file: "), mode = "r") as ifile:
         reader = DictReader(ifile, fieldnames = None, dialect = CSV_DIALECT)
         for row in reader:
-            fav = Favorite(**row)
-            print(fav)
+            favorite = Favorite(**row)
+            favorite_favorite(session, favorite)
+            print(f"favorited {favorite}.")
+            sleep(SECONDS_PER_FAV)
     print("all imported. >:3c")
